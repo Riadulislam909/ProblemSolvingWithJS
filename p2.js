fb177ef2c8a52c8748ea918ef6197f8e9d99a920
Riadul Islam <riadulislam814@gmail.com>
2023-08-30 02:40:44 +0600

    p2

---
+++ p2.js
@@ -0,0 +1,9 @@
+//How can you order names of students in your class sequentially
+
+const nameArray = ["Abid","Sajid","Raihan","Karim"];
+
+let ordered = nameArray.sort();
+console.log(ordered);
+
+
+
