02d0cbdd74475836729b8cb78ff9ad657f046b11
Riadul Islam <riadulislam814@gmail.com>
2023-08-30 14:38:09 +0600

    Extract the unique numbers in an array

---
+++ p7.js
@@ -0,0 +1,9 @@
+//How would you extract the unique numbers in an array
+
+const numberArray = [1,3,2,2,2,5,3,5,6,9,8,7];
+
+let unique = numberArray.filter(function(value, index){
+    return (numberArray.indexOf(value) === index);
+});
+
+console.log(unique);
